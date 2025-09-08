b4c22c3c24c4e5be7d2e1d7e91083b1c7a244627
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-08 12:58:56 +0200

    Import 2 functions into initial-page-load script

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,7 +1,11 @@
 import { createHeroSection } from "./hero-section";
+import { createRestaurantContent } from "./restaurant-content";
+import { createRestaurantInfo } from "./restaurant-info";
 
 function loadPage() {
   createHeroSection();
+  createRestaurantContent();
+  createRestaurantInfo();
 }
 
 export { loadPage };
