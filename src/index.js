2df1870dbe0c2e31f15ff7cb88e0711ca84f10c5
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-08 13:39:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "./styles.css";
 import { loadPage } from "./initial-page-load";
+import { clearContent } from "./clearContent";
+
+const homeBtn = document.querySelector("#homeBtn");
+const menuBtn = document.querySelector("#menuBtn");
+const aboutBtn = document.querySelector("#aboutBtn");
+
+homeBtn.addEventListener("click", () => {
+  clearContent();
+});
+menuBtn.addEventListener("click", () => {
+  clearContent();
+});
+aboutBtn.addEventListener("click", () => {
+  clearContent();
+});
 
 loadPage();
