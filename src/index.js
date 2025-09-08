444046c196a4102d3ca96184da4dece1d1a47780
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-08 13:41:11 +0200

    Update the homeBtn event listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { loadPage } from "./initial-page-load";
 import { clearContent } from "./clearContent";
+import { createHomepage } from "./createHomepage";
 
 const homeBtn = document.querySelector("#homeBtn");
 const menuBtn = document.querySelector("#menuBtn");
@@ -8,6 +9,7 @@ const aboutBtn = document.querySelector("#aboutBtn");
 
 homeBtn.addEventListener("click", () => {
   clearContent();
+  createHomepage();
 });
 menuBtn.addEventListener("click", () => {
   clearContent();
