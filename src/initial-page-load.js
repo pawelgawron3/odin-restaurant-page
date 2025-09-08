3b7e53862ca07c9cfeb03f1d9de0045cea71c84c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-08 13:14:12 +0200

    Update the script with footer.js file

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,11 +1,13 @@
 import { createHeroSection } from "./hero-section";
 import { createRestaurantContent } from "./restaurant-content";
 import { createRestaurantInfo } from "./restaurant-info";
+import { createFooter } from "./footer";
 
 function loadPage() {
   createHeroSection();
   createRestaurantContent();
   createRestaurantInfo();
+  createFooter();
 }
 
 export { loadPage };
